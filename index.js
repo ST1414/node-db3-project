434b46e223a567226623e704341d89606b006cae
ST1414 <sterry1414@gmail.com>
2021-12-08 12:27:23 -0800

    find query completed

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+// 4.2.3 Multi-Table Queries (JOINS) - Project
+// Wed. Dec. 8, 2021
+
 const server = require('./api/server.js');
 
 const PORT = process.env.PORT || 9000;
@@ -5,3 +8,7 @@ const PORT = process.env.PORT || 9000;
 server.listen(PORT, () => {
   console.log(`Listening on port ${PORT}...`);
 });
+
+server.get('/', (req, res) => {
+  res.send('<h1>4.2.3 - Multi-Table Queries Project</h1>');
+})
